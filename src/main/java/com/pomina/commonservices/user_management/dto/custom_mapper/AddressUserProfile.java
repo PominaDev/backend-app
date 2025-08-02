a1f7f5c8eac17722e8ae49cc57be5928c36fb067
NMinhNam <minhnam1810@gmail.com>
2025-08-02 16:03:08 +0700

    change response for user profile

---
+++ src/main/java/com/pomina/commonservices/user_management/dto/custom_mapper/AddressUserProfile.java
@@ -0,0 +1,18 @@
+package com.pomina.commonservices.user_management.dto.custom_mapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddressUserProfile {
+    private String address01;
+    private String address02;
+    private String address03;
+    private String address04;
+    private String address05;
+}
