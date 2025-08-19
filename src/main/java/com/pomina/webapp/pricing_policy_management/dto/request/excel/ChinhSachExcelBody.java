74261d4c5973c3c04b9ff732febf07edfa47cd58
NMinhNam <minhnam1810@gmail.com>
2025-08-19 21:45:51 +0700

    feat(Category): change response api /api/v1/categories

--- src/main/java/com/pomina/webapp/pricing_policy_management/dto/request/excel/ChinhSachExcelBody.java
+++ src/main/java/com/pomina/webapp/pricing_policy_management/dto/request/excel/ChinhSachExcelBody.java
@@ -13,52 +13,36 @@ import java.util.List;
 @Setter
 @Builder
 public class ChinhSachExcelBody {
-    @JsonProperty("ten_san_pham")
     private String tenSanPham;
 
-    @JsonProperty("size")
     private Double size;
 
-    @JsonProperty("gia_bang")
     private Double giaBang;
 
-    @JsonProperty("ck_thanh_toan")
     private Double ckThanhToan;
 
-    @JsonProperty("cs_trong_thang")
     private Double csTrongThang;
 
-    @JsonProperty("ck_cs_1")
     private Double ckCs1;
 
-    @JsonProperty("ck_cs_2")
     private Double ckCs2;
 
-    @JsonProperty("ck_cs_3")
     private Double ckCs3;
 
-    @JsonProperty("ck_cs_4")
     private Double ckCs4;
 
-    @JsonProperty("ck_cs_5")
     private Double ckCs5;
 
-    @JsonProperty("ck_cs_6")
     private Double ckCs6;
 
-    @JsonProperty("ck_cs_7")
     private Double ckCs7;
 
-    @JsonProperty("ck_cs_8")
     private Double ckCs8;
 
-    @JsonProperty("ck_cs_9")
     private Double ckCs9;
 
-    @JsonProperty("ck_cs_10")
     private Double ckCs10;
 
-    @JsonProperty("gia_met")
     private Double giaMet;
 
     @JsonIgnore
--- src/main/java/com/pomina/webapp/pricing_policy_management/dto/request/excel/ChinhSachExcelHeader.java
+++ src/main/java/com/pomina/webapp/pricing_policy_management/dto/request/excel/ChinhSachExcelHeader.java
@@ -1,6 +1,5 @@
 package com.pomina.webapp.pricing_policy_management.dto.request.excel;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,21 +8,15 @@ import lombok.Setter;
 @Setter
 @Builder
 public class ChinhSachExcelHeader {
-    @JsonProperty("ten_chinh_sach")
     private String tenChinhSach;
 
-    @JsonProperty("ngay_bat_dau")
     private String ngayBatDau;
 
-    @JsonProperty("ngay_ket_thuc")
     private String ngayKetThuc;
 
-    @JsonProperty("phong_ban")
     private String phongBan;
 
-    @JsonProperty("nguoi_lap")
     private String nguoiLap;
 
-    @JsonProperty("u_description")
     private String description;
 }
