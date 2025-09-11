99fd1d2a478302774c985b70107d7eab1a5656e7
ngxuanhainam612 <ngxuanhainam.com>
2025-09-11 21:43:47 +0700

    Commit01_HaiNam: add message to ScanProductWarrantyResponseDto (when scaned location over 2000m)

--- src/main/java/com/pomina/app/product_warranty_activation_scanning/service/impl/ScanProductWarrantyServiceImpl.java
+++ src/main/java/com/pomina/app/product_warranty_activation_scanning/service/impl/ScanProductWarrantyServiceImpl.java
@@ -3,6 +3,7 @@ package com.pomina.app.product_warranty_activation_scanning.service.impl;
 import com.pomina.app.product_warranty_activation_scanning.dto.request.ScanProductWarrantyRequestDto;
 import com.pomina.app.product_warranty_activation_scanning.dto.response.ScanProductWarrantyResponseDto;
 import com.pomina.app.product_warranty_activation_scanning.service.ScanProductWarrantyService;
+import com.pomina.common.enums.ScanProductWarrantyMessage;
 import com.pomina.common.exception.AppException;
 import com.pomina.common.exception.ErrorCode;
 import com.pomina.commonservices.location.dto.request.LocationRequestDto;
@@ -67,10 +68,12 @@ public class ScanProductWarrantyServiceImpl implements ScanProductWarrantyServic
             }
 
             warrantyInfoHistory = warrantyMapper.findWarrantyDetailByMaCuonTon(maCuonTon);
+            String message = isValidLocation ? "" : ScanProductWarrantyMessage.INVALID.getMessage();
 
             return ScanProductWarrantyResponseDto.builder()
                     .resultInserted(resultInserted)
                     .productDetailResponseDto(warrantyInfoHistory)
+                    .message(message)
                     .build();
         } else {
             // quét lần 2
