fd1f40cb2722c4930fe9ca634f8490888914ee76
quynhnhupham294 <quynhpnps39894@gmail.com>
2025-09-13 17:32:44 +0700

    change profile active to dev

--- src/main/java/com/pomina/webapp/product_warranty_activation_tracking/controller/ProductWarrantyController.java
+++ src/main/java/com/pomina/webapp/product_warranty_activation_tracking/controller/ProductWarrantyController.java
@@ -40,8 +40,11 @@ public class ProductWarrantyController {
     public ResponseEntity<ApiResponse<PageResponse<WarrantyInfoHistory>>> filterWarrantyInfoHistory(@Valid @ModelAttribute PageRequest pageRequest,
                                                                                                     @RequestParam(required = false) List<String> filter,
                                                                                                     @RequestParam(required = false) String isValid,
+                                                                                                    @RequestParam(required = false) String status,
                                                                                                     @RequestParam(required = false) String sort) {
         Boolean isValidBool = (isValid == null || isValid.isBlank()) ? null : Boolean.parseBoolean(isValid);
-        return ResponseHandler.success(warrantyService.filterWarrantyInfoHistory(pageRequest, true, filter, isValidBool, sort));
+
+
+        return ResponseHandler.success(warrantyService.filterWarrantyInfoHistory(pageRequest, true, filter, isValidBool, status, sort));
     }
 }
