16afe2f9adf58ac4f53c4192b3f2e962b4636acd
NMinhNam <minhnam1810@gmail.com>
2025-09-19 01:04:57 +0700

    feat(PricingPolicy): update api preview file - response type Double not upper value

--- src/main/java/com/pomina/webapp/pricing_policy_management/mapper/ChinhSachGiaBanChildMapper.java
+++ src/main/java/com/pomina/webapp/pricing_policy_management/mapper/ChinhSachGiaBanChildMapper.java
@@ -2,6 +2,7 @@ package com.pomina.webapp.pricing_policy_management.mapper;
 
 import com.pomina.common.mapper.BaseMapper;
 import com.pomina.common.model.PageRequest;
+import com.pomina.webapp.pricing_policy_management.dto.response.ChinhSachGiaBanChildResponseDto;
 import com.pomina.webapp.pricing_policy_management.entity.ChinhSachGiaBanChild;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -12,10 +13,10 @@ import java.util.List;
 public interface ChinhSachGiaBanChildMapper extends BaseMapper<ChinhSachGiaBanChild> {
     int batchInsert(List<ChinhSachGiaBanChild> chinhSachGiaBanChildList);
 
-    List<ChinhSachGiaBanChild> findByUChinhSachParentId(@Param("offset") int offset,
-                                                        @Param("limit") int limit,
-                                                        @Param("paging") PageRequest pageRequest,
-                                                        @Param("uChinhSachParentId") int uChinhSachParentId);
+    List<ChinhSachGiaBanChildResponseDto> findByUChinhSachParentId(@Param("offset") int offset,
+                                                                   @Param("limit") int limit,
+                                                                   @Param("paging") PageRequest pageRequest,
+                                                                   @Param("uChinhSachParentId") int uChinhSachParentId);
 
     int countFindByUChinhSachParentId(@Param("uChinhSachParentId") int uChinhSachParentId);
 }
